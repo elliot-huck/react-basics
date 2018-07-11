3472a3280cc043c1300d52738be4658f012b5722
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-11 11:18:35 -0500

    Added a Brentwood location

--- kennel-company/src/LocationList.js
+++ kennel-company/src/LocationList.js
@@ -9,6 +9,8 @@ export default class LocationList extends Component {
         <h5>500 Puppy Way</h5>
         <h4>Nashville South Location</h4>
         <h5>342 Pets Blvd</h5>
+        <h4>Brentwood Location</h4>
+        <h5>5533 S. Brentwood Ct</h5>
       </article>
     )
   }
