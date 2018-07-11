a9be7bbda5cf17ebf57fbebc0c2e5596d12838fe
Elliot Huck <39445681+elliot-huck@users.noreply.github.com>
2018-07-11 11:13:01 -0500

    Added LocationList to display all locations

---
+++ kennel-company/src/LocationList.js
@@ -0,0 +1,15 @@
+
+import React, { Component } from 'react';
+
+export default class LocationList extends Component {
+  render() {
+    return (
+      <article>
+        <h4>Nashville North Location</h4>
+        <h5>500 Puppy Way</h5>
+        <h4>Nashville South Location</h4>
+        <h5>342 Pets Blvd</h5>
+      </article>
+    )
+  }
+}
